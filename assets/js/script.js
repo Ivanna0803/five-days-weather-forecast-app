fdfe01191edab4b06930d01f8552f6adc16f14ae
Ivanna <ivanna.kuliak@gmail.com>
2023-03-07 23:01:25 +0000

    Build new OpenWeather URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// OpenWeather API key""
+// OpenWeather API key
 const APIKey = "219c2e541d0b04c9a6bae69e7e337991";
 // let todayForecast = $("#today");
 // let futureForecast = $("#forecast");
@@ -6,23 +6,27 @@ const APIKey = "219c2e541d0b04c9a6bae69e7e337991";
 // Takes data from URL
 const params = (new URL(document.location)).searchParams;
 const searchInput = params.get('search-input');
+let currentDate = moment().format("DD/MM/YY");
 
-if (searchInput) {
+function searchCityWeatherForecast (searchInput) {
     // Building the URL to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIKey;
-    
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + APIKey;
+
     $.ajax({
          url: queryURL, 
          method: "GET"
-        }).then(function(response) {        
-            let city = response.name;
-            let tempCelsius = Math.floor(response.main.temp - 273.15);
-            let windSpeed = response.wind.speed;
-            let humidity = response.main.humidity;
+        }).then(function(response) {
+            let city = searchInput;        
+            let tempCelsius = Math.floor(response.list[0].main.temp - 273.15);
+            let windSpeed = response.list[0].wind.speed;
+            let humidity = response.list[0].main.humidity;
 
-            $('.city').text(city);
+            $('.city').text(city + " (" + currentDate + ")");
             $('.temp').text("Temp: " + tempCelsius + "°C");
             $('.wind').text("Wind: " + windSpeed + " KPH");
             $('.humidity').text("Humidity: " + humidity + "%");
-        });    
+            console.log(queryURL);
+        })       
 }
+
+searchCityWeatherForecast(searchInput);
